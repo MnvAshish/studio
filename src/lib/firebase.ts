1300434ca393eebc87d8153c654a01daaf5a5038
Ashish M <ashsenpai167@gmail.com>
2025-11-14 15:20:44 +0000

    TypeError: authorizedDomains is not iterable     at _validateOrigin (htt

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,18 @@
 import { initializeApp, getApps, getApp, FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+const firebaseConfigString = process.env.NEXT_PUBLIC_FIREBASE_CONFIG;
+if (!firebaseConfigString) {
+  throw new Error('NEXT_PUBLIC_FIREBASE_CONFIG is not set in the environment');
+}
+
+let firebaseConfig: FirebaseOptions;
+try {
+  firebaseConfig = JSON.parse(firebaseConfigString);
+} catch (e) {
+    throw new Error('Failed to parse NEXT_PUBLIC_FIREBASE_CONFIG');
+}
+
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
